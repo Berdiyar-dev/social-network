7ea8efb18d29fa571578228719ecfbada04d77ea
berdiyar_developer <135032538+Berdiyar-dev@users.noreply.github.com>
2023-09-04 07:29:47 +0500

    Update settings.py
    change

--- config/settings.py
+++ config/settings.py
@@ -130,11 +130,13 @@ USE_TZ = True
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 if not DEBUG:
-    STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static/']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media/'
 
